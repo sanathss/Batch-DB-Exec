d4e3738943f5e90185c32a610824b22570dd9450
Sanath Samarasekara <sanathss.94@gmail.com>
2020-04-08 12:55:17 +1200

    Add confirmation message function. Include confirmation for dbConfig.

--- src/commands.py
+++ src/commands.py
@@ -5,6 +5,17 @@ from tkinter.filedialog import askdirectory
 def getFileDirectory():
     return askdirectory()
 
+def confirmationMessage():
+    while(True):
+        value = input("Confirm (Y/N): ") 
+        if value in ['Y', 'y']:
+            return True
+        elif value in ['N', 'n']:
+            return False
+
 def startExecution():
     #Confirm Database Connection Details
+    print('Database Connection details:')
     print(readDBConfigDetails())
+    if confirmationMessage() == False:
+        return 
