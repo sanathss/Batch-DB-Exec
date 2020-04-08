2d84cfce68d3d7089781d830271e5683411f70e0
Sanath Samarasekara <sanathss.94@gmail.com>
2020-04-08 14:47:22 +1200

    Welcome message

--- src/main.py
+++ src/main.py
@@ -3,6 +3,8 @@ import pandas as pd
 from commands import startExecution
 
 def main():
+    print('Batch DB Exec')
+    print('-----------------')
     startExecution()
 
 if __name__ == "__main__":
